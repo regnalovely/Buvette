50dad3dc6a5586d9e92393dbdae2181ce7ff17ab
Regna Lovely <regna.lovely@gmail.com>
2018-11-05 08:21:02 -0400

    Initial Commit

---
+++ Buvette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Buvette
+//
+//  Created by etudiant on 05/11/2018.
+//  Copyright © 2018 L3P-IEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
